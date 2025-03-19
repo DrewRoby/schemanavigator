f7f67886e72817da1c89b17061c529d624c1cc36
Roby <drew.roby@cgi.com>
2025-03-18 22:49:05 -0500

    updated urls

--- schemanavigator/settings.py
+++ schemanavigator/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.conf import settings
+from django.conf.urls.static import static
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
